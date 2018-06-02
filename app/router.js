d4becfc0ca5d06ec1147ccc40f5ef39d910366a5
Zen Pandawan <srsmith.pro@gmail.com>
2018-06-01 21:50:01 -0400

    Create Base Ember Project With Some Routes

---
+++ app/router.js
@@ -0,0 +1,23 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('orgs');
+
+  this.route('org', function() {
+    this.route('emberjs', function() {
+      this.route('repos');
+      this.route('ember.js', function() {
+        this.route('issues');
+        this.route('contributors');
+      });
+    });
+  });
+});
+
+export default Router;
