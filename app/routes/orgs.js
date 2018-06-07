98fa1bb489bd08b2d870173b7e92a8ebf0d9f5e6
Zen Pandawan <srsmith.pro@gmail.com>
2018-06-07 11:48:34 -0400

    FEAT: Add List of Orgs for /orgs Route
    To have a list of dummy orgs as content for orgs page.
    
    References:
    Issue #3

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(){
+        return [
+            'Facebook',
+            'Netflix',
+            'Yahoo',
+            'emberjs'
+        ];
+    }
+
+
 });
